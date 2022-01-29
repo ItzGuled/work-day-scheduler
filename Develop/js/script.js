9d2d8ef5b31fa666af1b589e9d03a038eb684b17
itzguled <itzguled55337@gmail.com>
2022-01-28 19:48:51 -0600

    local store

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,21 +1,26 @@
 // declaring variables
 var now = moment();
-var jumbo = now.format("dddd, MMMM D")
-$("#currentDay").text(jumbo)
-var saveBtn = document.querySelector(".saveBtn");
-
-
+var jumbo = now.format("dddd, MMMM D");
+$("#currentDay").text(jumbo);
 
 // function for saving the text area
-var saveTasks = function () {
-    var textBox = document.querySelector(".description").value;
-    localStorage.setItem("description", textBox);
-    
+var saveTasks = function (event) {
+  console.log('line 8', event);
+  const divId = event.target.id.match(/\d+/g)[0];
+  console.log("line 10", divId);
+  var textBox = $(`#${divId}`).value;
+  localStorage.setItem("description", textBox);
+  console.log(event.target);
 };
 
-  
-  saveBtn.addEventListener("click", saveTasks)
+var addListeners = function () {
+  for (var i = 0; i <= 8; i++) {
+    const divEl = $(`#${i}`);
+    var button = divEl.children("button");
+    $(button[0]).attr("id", `btn${i}`);
+    button[0].addEventListener("click", saveTasks);
+  }
+};
+// saveBtn.addEventListener("click", saveTasks);
 
-
-
-  
+addListeners();
